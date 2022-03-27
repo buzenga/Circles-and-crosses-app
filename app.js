37b5d4e5d5780d946025bd1d4ddb4292c677b0ae
buzenga <100876928+buzenga@users.noreply.github.com>
2022-03-27 14:27:02 +0200

    Add data atributes to each clicked gamefield
    Implemented improvment causing change of color on each clicked game field. It goes together with setting circle/cross data-atribute.

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 const fields = document.querySelectorAll(".field");
+const start = document.querySelector("button");
 
-fields.forEach(field => 
-   field.classList.add("hover_on")
-)
 
-fields.forEach(field => field.addEventListener("click", function handle(e) {
-   field.removeEventListener("click",handle);
-   field.classList.remove("hover_on")
-   field.style.backgroundColor = "green";
-}))
+// const test = document.querySelector(".field");
+// test.addEventListener("click", () => {
+//    test.dataset.figure = "siema";
+// })
+
+
+start.addEventListener("click", () => {
+   singleGame()
+   start.style.display ="none";
+
+})
+
+// FUNCTIONS
+function singleGame(){
+   let figureSwitch = "circle";
+   
+   fields.forEach(field => 
+      field.classList.add("hover_on")
+   )
+
+   fields.forEach(field => field.addEventListener("click", function handle(e) {
+      field.removeEventListener("click",handle);
+      field.classList.remove("hover_on")
+      // field.dataset.figure = figureSwitch;
+
+      console.log("przed: " + figureSwitch)
+      switch(figureSwitch) {
+         case "circle":
+            field.style.backgroundColor = "green";
+            field.dataset.figure = figureSwitch;
+            figureSwitch = "cross"; 
+            break;
+         case "cross":
+            field.style.backgroundColor = "red";
+            field.dataset.figure = figureSwitch;
+            figureSwitch = "circle"; 
+            break;    
+      }
+
+      console.log("po: " + figureSwitch) 
+}))}
+
 
