18d86a5de83fd2b3d0d1029d5f7bba64fb7c9c05
buzenga <100876928+buzenga@users.noreply.github.com>
2022-03-27 15:17:20 +0200

    Add checking result mechanism
    The code allows to check, which player won the game

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const fields = document.querySelectorAll(".field");
 const start = document.querySelector("button");
 
+const one = document.querySelector(".one")
+const two = document.querySelector(".two")
+const three = document.querySelector(".three")
+const four = document.querySelector(".four")
+const five = document.querySelector(".five")
+const six = document.querySelector(".six")
+const seven = document.querySelector(".seven")
+const eight = document.querySelector(".eight")
+const nine = document.querySelector(".nine")
+
+
 
 // const test = document.querySelector(".field");
 // test.addEventListener("click", () => {
@@ -40,8 +51,46 @@ function singleGame(){
             figureSwitch = "circle"; 
             break;    
       }
+// ------------------------------------- CROSSES WIN ------------------------------------------
+      if ((one.dataset.figure == two.dataset.figure && three.dataset.figure == two.dataset.figure && two.dataset.figure == "cross") ||
 
-      console.log("po: " + figureSwitch) 
+      (four.dataset.figure == five.dataset.figure && six.dataset.figure == five.dataset.figure && five.dataset.figure == "cross") ||
+      
+      (seven.dataset.figure == eight.dataset.figure && eight.dataset.figure == nine.dataset.figure && nine.dataset.figure == "cross") ||
+
+      (one.dataset.figure == four.dataset.figure && four.dataset.figure == seven.dataset.figure && seven.dataset.figure == "cross") ||
+
+      (two.dataset.figure == five.dataset.figure && five.dataset.figure == eight.dataset.figure && five.dataset.figure == "cross") ||
+
+      (three.dataset.figure == six.dataset.figure && six.dataset.figure == nine.dataset.figure && six.dataset.figure == "cross") ||
+
+      (one.dataset.figure == five.dataset.figure && five.dataset.figure == nine.dataset.figure && five.dataset.figure == "cross") ||
+
+      (seven.dataset.figure == five.dataset.figure && five.dataset.figure == three.dataset.figure && five.dataset.figure == "cross")
+
+      ) {
+         alert("o kurwa wygral krzyzk");
+      }
+// ------------------------------------- CIRCLES WIN --------------------------------------
+      if ((one.dataset.figure == two.dataset.figure && three.dataset.figure == two.dataset.figure && two.dataset.figure == "circle") ||
+
+      (four.dataset.figure == five.dataset.figure && six.dataset.figure == five.dataset.figure && five.dataset.figure == "circle") ||
+      
+      (seven.dataset.figure == eight.dataset.figure && eight.dataset.figure == nine.dataset.figure && nine.dataset.figure == "circle") ||
+
+      (one.dataset.figure == four.dataset.figure && four.dataset.figure == seven.dataset.figure && seven.dataset.figure == "circle") ||
+
+      (two.dataset.figure == five.dataset.figure && five.dataset.figure == eight.dataset.figure && five.dataset.figure == "circle") ||
+
+      (three.dataset.figure == six.dataset.figure && six.dataset.figure == nine.dataset.figure && six.dataset.figure == "circle") ||
+
+      (one.dataset.figure == five.dataset.figure && five.dataset.figure == nine.dataset.figure && five.dataset.figure == "circle") ||
+
+      (seven.dataset.figure == five.dataset.figure && five.dataset.figure == three.dataset.figure && five.dataset.figure == "circle")
+
+      ) {
+         alert("o kurwa kolko wygralo");
+      }
 }))}
 
 
